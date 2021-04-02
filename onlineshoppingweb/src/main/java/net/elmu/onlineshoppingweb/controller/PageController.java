c9c2bdacf25157232da3d038cb491dacab016e84
Tadjibaev Elmurodjon <eltadjiabev@gmail.com>
2021-04-02 11:40:35 +0900

    complited assets part

---
+++ onlineshoppingweb/src/main/java/net/elmu/onlineshoppingweb/controller/PageController.java
@@ -0,0 +1,80 @@
+package net.elmu.onlineshoppingweb.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+//import net.elmu.shoppingbackend.dao.CategoryDAO;
+//import net.elmu.shoppingbackend.dto.Category;
+
+@Controller
+public class PageController {
+
+	//@Autowired
+	//private CategoryDAO categoryDAO;
+
+	@RequestMapping(value = {"/", "/home", "/index"})
+	public ModelAndView index() {
+		
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("title", "Home");
+		// passing the list of categories
+		//mv.addObject("categories", categoryDAO.list());
+		mv.addObject("userClickHome", true);
+		
+		return mv;
+	}
+	
+	@RequestMapping(value = "/about")
+	public ModelAndView about() {
+		
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("title", "About Us");
+		mv.addObject("userClickAbout", true);
+		
+		return mv;
+	}
+	
+	@RequestMapping(value = "/contact")
+	public ModelAndView contact() {
+		
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("title", "Contact Us");
+		mv.addObject("userClickContact", true);
+		
+		return mv;
+	}
+	/*
+	@RequestMapping(value = {"/show/all/products"})
+	public ModelAndView showAllProducts() {
+		
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("title", "All Products");
+		// passing the list of categories
+		mv.addObject("categories", categoryDAO.list());
+		mv.addObject("userClickAllProducts", true);
+		
+		return mv;
+	}
+	
+	@RequestMapping(value = {"/show/category/{id}/products"})
+	public ModelAndView showCategoryProducts(@PathVariable("id") int id) {
+		
+		ModelAndView mv = new ModelAndView("page");
+		
+		// categoryDao to fetch a single category
+		Category category = categoryDAO.get(id);
+		
+		mv.addObject("title", category.getName());
+		
+		// passing the list of categories
+		mv.addObject("categories", categoryDAO.list());
+		
+		// passing the single category object
+		mv.addObject("category", category);
+		
+		mv.addObject("userClickCategoryProducts", true);
+		
+		return mv;
+	}*/
+}
